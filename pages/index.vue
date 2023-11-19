ff0a31ee929da20b902781c91606763e01b18c1d
ekaterina <ekaterinavasileva01@gmail.com>
2023-11-19 20:23:01 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+
+<div>
+  <h2>
+    Home
+  </h2>
+</div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style scoped>
+h2{
+  margin:0;
+  font-size:36px;
+}
+
+p{
+  margin:0;
+}
+</style>
